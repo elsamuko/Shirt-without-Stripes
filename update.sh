01263658640c41be74efbf9394f4e2f3fe3aff74
Melvil GUILLAUME <melvil.guillaume@gmail.com>
2020-04-22 10:21:59 +0200

    fix error in benchmark
    The `bing` site would fetch results for `shirts without stripes` instead of `shirt without stripes`. This would result in an erroneous benchmark

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ cd "$DEST" || exit
 
 declare -A SITES
 SITES["amazon"]="https://www.amazon.com/s?k=shirt+without+stripes"
-SITES["bing"]="https://www.bing.com/images/search?q=shirts+without+stripes"
+SITES["bing"]="https://www.bing.com/images/search?q=shirt+without+stripes"
 SITES["google"]="https://www.google.com/search?q=shirt+without+stripes&tbm=isch"
 
 for COMPANY in "${!SITES[@]}"; do
