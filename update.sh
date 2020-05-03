8eaf86c92a5bc42ca591170b303d0fb2667fedf0
Samuel Albrecht <elsamuko@gmail.com>
2020-05-03 17:57:43 +0200

    add: logging to update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # get current state and save them in a subfolder with date
 
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TODAY=$(date +20%y-%m-%d)
 DEST="screenshots/$TODAY"
+
+function log {
+    printf "%s : %s\n" "$(date +'%F %T' )" "$@" | tee -a "$SCRIPT_DIR/update.log"
+}
 
 mkdir -p "$DEST"
 cd "$DEST" || exit
@@ -13,11 +18,11 @@ SITES["bing"]="https://www.bing.com/images/search?q=shirt+without+stripes"
 SITES["google"]="https://www.google.com/search?q=shirt+without+stripes&tbm=isch"
 
 for COMPANY in "${!SITES[@]}"; do
-    echo "Getting current state of $COMPANY"
-    chromium-browser \
+    log "Getting current state of $COMPANY"
+    log "$( chromium-browser \
         --headless \
         --disable-gpu \
         --hide-scrollbars \
         --window-size=1000,1000 \
-        --screenshot="$COMPANY.png" "${SITES[$COMPANY]}" 2> /dev/null
+        --screenshot="$COMPANY.png" "${SITES[$COMPANY]}" 2>&1 )"
 done
